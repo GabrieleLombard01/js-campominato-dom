9726454663550707614429e46220623b4f15ee87
GabrieleLombard01 <gabriellombardo2001@gmail.com>
2023-06-08 21:33:49 +0200

    difficulty fixed

--- js/script.js
+++ js/script.js
@@ -5,6 +5,7 @@ console.log('JS OK')
 // RECUPERO gli elementi dal DOM
 const grid = document.getElementById('grid');
 const playButton = document.getElementById('play-button');
+const levelSelect = document.getElementById('difficulty');
 
 
 //Funzione start-game
@@ -16,23 +17,52 @@ function startGame(){
     
     //Svuoto la griglia
     grid.innerHTML = '';
+
+    // Recupero il livello scelto
+    const level = levelSelect.value;
     
+    //Calcolo le celle totali
+    let rows;
+    let cols;
+
+    switch(level) {
+        case 'hard':
+            rows = 7;
+            cols = 7;
+            break;
+        case 'normal':
+            rows = 9;
+            cols = 9;
+            break;
+        case 'easy':
+            default:
+            rows = 10;
+            cols = 10;
+            break;
+    };
     
     //Funzione per creare la cella
-    const createCell = (cellNumber) => {
+    const createCell = (cellNumber, level) => {
+
         const cell = document.createElement('div');
-        cell.className = 'cell';
+        cell.classList.add('cell', level);
+
+        
+
         cell.append(cellNumber);
+
         return cell;
     };
+
+    const totalCells = rows * cols;
     
     //! LOGICA DI GIOCO EFFETTIVA:
 
     //Generare la griglia
-    for(let i = 1; i <= 100; i++) {
+    for(let i = 1; i <= totalCells; i++) {
 
         //CREO la cella
-        const cell = createCell(i);
+        const cell = createCell(i, level);
 
         //IN ASCOLTO sulla cella
         cell.addEventListener('click',() => {
